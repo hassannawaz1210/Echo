4bacfe5785a5be460d225d0627bb184d0b57cd99
hassannawaz1210 <hassannawaz1210@gmail.com>
2023-11-11 22:18:15 +0500

    delete
    deleting all files to reupload a newly built version

--- gui_client/Dependencies/mongo-cxx-driver/lib/cmake/bsoncxx-3.8.0/bsoncxx-config.cmake
+++
@@ -1,4 +0,0 @@
-include(CMakeFindDependencyMacro)
-
-find_dependency(Boost 1.56.0 REQUIRED)
-include("${CMAKE_CURRENT_LIST_DIR}/bsoncxx_targets.cmake")
--- server/server.cpp
+++
@@ -1,9 +0,0 @@
-// Chat Server
-#include "servernetwork.hpp"
-
-int main(int argc, char *argv[])
-{
-     ServerNetwork server_network(2525);
-     server_network.Run();
-     return 0;
-}
